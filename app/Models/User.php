694e6dbfe1716e8ee5e8ba4dab1458f86fdbc829
Krasimira Georgieva <krassi77bg@gmail.com>
2023-02-13 01:42:23 +0200

    Defined a relationship, implemented method addToBookmarkLibraries

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -41,4 +42,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function bookmarkLibraries(): MorphToMany
+    {
+        return $this->morphedByMany(Library::class, 'object', 'bookmarks')->withTimestamps();
+    }
+
+    public function addToBookmarkLibraries(Library $library): void
+    {
+        if (!$this->bookmarkLibraries()->where('bookmarks.object_id', $library->id)->exists()) {
+            $this->bookmarkLibraries()->attach($library);
+        }
+    }
 }
