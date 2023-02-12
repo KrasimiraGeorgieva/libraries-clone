a2695d68bd7645f9f1352537a8d399899b8237d7
Krasimira Georgieva <krassi77bg@gmail.com>
2023-02-12 22:19:41 +0200

    created database migrations

---
+++ database/migrations/2023_02_12_192619_create_bookmarks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('bookmarks', function (Blueprint $table) {
+            $table->id();
+            $table->string('object_type');
+            $table->integer('object_id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookmarks');
+    }
+};
