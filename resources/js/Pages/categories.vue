de155a41e0375e6bde0b5cf1d1e69c5e44131359
Krasimira Georgieva <krassi77bg@gmail.com>
2023-02-14 16:32:53 +0200

    Added error messages

--- resources/js/Pages/categories.vue
+++ resources/js/Pages/categories.vue
@@ -9,6 +9,14 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
+                    <div
+                        class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md my-3" role="alert" v-if="$page.props.flash.message">
+                        <div class="flex">
+                            <div>
+                                <p class="text-sm">{{ $page.props.flash.message }}</p>
+                            </div>
+                        </div>
+                    </div>
                     <button
                         @click="openForm()"
                         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-3">Create New
@@ -86,6 +94,7 @@ export default {
             this.isFormOpen = true;
             this.isFormEdit = !!item;
             this.formObject = item ? item : defaultFormObject;
+            this.$page.props.errors = {};
         }
     }
 }
